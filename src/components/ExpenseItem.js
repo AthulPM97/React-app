84a1da552a8d3ff2839a4e318c98ca590c16b4ca
athulpm <97athulpm@gmail.com>
2022-12-10 13:40:35 +0530

    Added ExpenseItem with basic jsx

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <h2>Expense Items</h2>
+            <p>Food <span>Rs 10</span></p>
+            <p>Petrol <span>Rs 100</span></p>
+            <p>Movie <span>Rs 200</span></p>
+        </div>
+    )
+}
+
+export default ExpenseItem;
