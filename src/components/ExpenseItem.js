59f605de0a340b4d69b929a1ca609703e27b527f
athulpm <97athulpm@gmail.com>
2022-12-10 15:56:17 +0530

    added location of expenditure

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,19 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 2, 28);
+    const description = "car insurance";
+    const price = 218;
+    const locationOfExpenditure = "Insurance office";
+
     return (
-        <div>
-            <h2>Expense Items</h2>
-            <p>Food <span>Rs 10</span></p>
-            <p>Petrol <span>Rs 100</span></p>
-            <p>Movie <span>Rs 200</span></p>
+        <div className='expense-item'>
+            <div>{expenseDate.toISOString()}</div>
+            <div className='expense-item__description'>
+                <h2>{description}</h2>
+                <div>{locationOfExpenditure}</div>
+                <div className='expense-item__price'>{`$ ${price}`}</div>
+            </div>
         </div>
     )
 }
