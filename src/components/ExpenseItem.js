4cb02ba75345a52ebb414a74145a0cad0bfc531b
athulpm <97athulpm@gmail.com>
2022-12-11 18:04:01 +0530

    Now using props and map to loop through array

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,14 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 2, 28);
-    const description = "car insurance";
-    const price = 218;
-    const locationOfExpenditure = "Insurance office";
-
+function ExpenseItem(props) {
+    
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>{description}</h2>
-                <div>{locationOfExpenditure}</div>
-                <div className='expense-item__price'>{`$ ${price}`}</div>
+                <h2>{props.title}</h2>
+                <div>{props.locationOfExpenditure}</div>
+                <div className='expense-item__price'>{`$ ${props.amount}`}</div>
             </div>
         </div>
     )
