ca8b80a89fe4f20f0e21dafdbe526d0bdb7d48a7
athulpm <97athulpm@gmail.com>
2022-12-13 12:57:20 +0530

    Folders organized and added arrow functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import Card from "./components/Card";
-import ExpenseItem from "./components/ExpenseItem";
+import Card from "./components/UI/Card";
+import ExpenseItem from "./components/Expenses/ExpenseItem";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: 'e1',
